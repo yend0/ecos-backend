d7bc3cf08b61833059cd8e8551ed1b4e495eefdc
Vadim Remezov <aspowersan@protonmail.com>
2025-01-13 18:50:35 +0300

    add UserModel

---
+++ src/ecos_backend/domain/user.py
@@ -0,0 +1,17 @@
+import datetime
+import uuid
+
+from dataclasses import dataclass
+
+from ecos_backend.common.interfaces import model
+
+
+@dataclass
+class UserModel(model.AbstractModel):
+    id: uuid.UUID
+    email: str
+    password: str = None
+    first_name: str = None
+    last_name: str = None
+    birth_date: datetime.date = None
+    image_url: str = None
