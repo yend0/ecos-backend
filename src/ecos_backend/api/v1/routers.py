93d8293d65b1e613a9f723838a625f33f1f8143c
Vadim Remezov <aspowersan@protonmail.com>
2025-01-13 18:42:01 +0300

    add homepage redirect

--- src/ecos_backend/api/v1/routers.py
+++ src/ecos_backend/api/v1/routers.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 from fastapi.routing import APIRouter
 
+from ecos_backend.common import config
+from ecos_backend.api.v1.controllers import homepage
 from ecos_backend.api.v1.controllers import user
 
 
@@ -9,5 +11,10 @@ class Tags(Enum):
     user: str = "User"
 
 
-api_router = APIRouter()
-api_router.include_router(user.router, prefix="/user", tags=[Tags.user])
+root = APIRouter()
+root.include_router(homepage.router)
+
+api_router_v1 = APIRouter(prefix=f"{config.URLPathsConfig.API_PREFIX}/v1")
+api_router_v1.include_router(user.router, prefix="/user", tags=[Tags.user])
+
+root.include_router(api_router_v1)
