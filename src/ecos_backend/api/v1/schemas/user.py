675095059fccc35a53d58dcb046c9c5b7491575b
Vadim Remezov <aspowersan@protonmail.com>
2025-01-13 18:39:38 +0300

    update UserResponseSchema

--- src/ecos_backend/api/v1/schemas/user.py
+++ src/ecos_backend/api/v1/schemas/user.py
@@ -5,18 +5,20 @@ from pydantic import BaseModel, ConfigDict, HttpUrl
 
 
 class UserBaseSchema(BaseModel):
-    phone_number: str
+    email: str
 
     model_config: ConfigDict = ConfigDict(extra="forbid")
 
 
-class UserCreateSchema(UserBaseSchema):
+class UserRequestCreateSchema(UserBaseSchema):
     password: str
 
 
-class UserSchema(UserBaseSchema):
+class UserResponseSchema(UserBaseSchema):
     id: uuid.UUID
     birth_date: datetime.date | None = None
     image_url: HttpUrl | None = None
+    first_name: str | None = None
+    last_name: str | None = None
 
     model_config: ConfigDict = ConfigDict(from_attributes=True)
