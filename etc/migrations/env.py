21e81c406c9d7e607fb9f7524d206ca81fba5f57
Vadim Remezov <aspowersan@protonmail.com>
2025-01-20 14:32:27 +0300

    delete print

--- etc/migrations/env.py
+++ etc/migrations/env.py
@@ -24,7 +24,6 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 
-print(mapper_registry.metadata)
 target_metadata = mapper_registry.metadata
 
 # other values from the config, defined by the needs of env.py,
